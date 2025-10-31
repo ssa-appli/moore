dcb2d8d98ce5e2917ed4a6644389ad74f90302fc
Mouhamed Hadiyatoulah <hadiyatoulah@outlook.com>
2025-10-31 14:01:29 +0000

    Updating allowed host

--- moore/settings.py
+++ moore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-iurbz1#+&n@vo@w7%$u)^-uvyl*ehc4#ucppaj0bov8nbhtqz@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["moore-0fz5.onrender.com"]
 
 
 # Application definition
